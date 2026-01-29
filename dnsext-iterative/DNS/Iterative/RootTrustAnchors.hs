916921c9f0e6dd9034d619a4aee8acca1b1d666b
Kei Hibino <ex8k.hibino@gmail.com>
2026-01-29 20:18:08 +0900

    dnsext-iterative: drop unused rootSepDS

--- dnsext-iterative/DNS/Iterative/RootTrustAnchors.hs
+++ dnsext-iterative/DNS/Iterative/RootTrustAnchors.hs
@@ -2,7 +2,6 @@
 
 module DNS.Iterative.RootTrustAnchors (
     rootSepDSs,
-    rootSepDS,
 ) where
 
 import DNS.SEC
@@ -15,14 +14,5 @@ rootSepDSs = [RD_DS 20326 RSASHA256 SHA256 digest]
   where
     digest =
         either (error . ("rootSepDSs: bad configuration: " ++)) id $
-            Opaque.fromBase16
-                "E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D"
-
-{-# DEPRECATED rootSepDS "use rootSepDSs instead of this" #-}
-rootSepDS :: RD_DS
-rootSepDS = RD_DS 20326 RSASHA256 SHA256 digest
-  where
-    digest =
-        either (error . ("rootSepDS: bad configuration: " ++)) id $
             Opaque.fromBase16
                 "E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D"
