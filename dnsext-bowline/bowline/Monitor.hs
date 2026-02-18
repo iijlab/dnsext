9024e8e1fda0acd5ed34dc1dd72a74e18e386947
Kei Hibino <ex8k.hibino@gmail.com>
2026-02-18 17:23:32 +0900

    monitor: loggingException with re-throwing

--- dnsext-bowline/bowline/Monitor.hs
+++ dnsext-bowline/bowline/Monitor.hs
@@ -10,7 +10,7 @@ import Control.Applicative ((<|>))
 import Control.Concurrent (forkFinally, getNumCapabilities, threadWaitRead)
 import Control.Concurrent.Async (waitSTM)
 import Control.Concurrent.STM (STM, atomically)
-import Control.Exception (SomeException, try)
+import Control.Exception (SomeException, throwIO, try)
 import Control.Monad
 import Data.ByteString.Builder
 import qualified Data.ByteString.Lazy.Char8 as BL
@@ -281,6 +281,13 @@ console conf env ctl@Control{..} GlobalCache{gcacheControl=CacheControl{..}} srv
             , ("help",            ("help", "show this help"))
             ]
 {- FOURMOLU_ENABLE -}
+
+loggingException :: (String -> IO a) -> IO b -> IO b
+loggingException logging action = try action >>= either left pure
+  where
+    -- SomeException: asynchronous exceptions are re-thrown
+    left :: SomeException -> IO b
+    left e = logging ("Monitor: un-expected exception: " ++ show e) >> throwIO e
 
 withWait :: STM a -> IO b -> IO (Either a b)
 withWait qstm blockAct =
